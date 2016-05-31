47205c66262a70050788fa0b25911cbead443170
Adam DiCarlo <adam.dicarlo@gmail.com>
2016-05-31 12:51:17 -0700

    new (splash page) site!

---
+++ src/splash.js
@@ -0,0 +1,71 @@
+import domready from 'domready'
+import raf from 'raf'
+import {ColorFactory} from 'colorfactory'
+import 'normalize.css/normalize.css'
+import './splash.css'
+
+const canvas = document.createElement('canvas')
+let width, height
+let about = {
+  el: undefined,
+  width: undefined,
+  height: undefined
+}
+
+domready(() => {
+  canvas.style = 'position: fixed; left: 0; top: 0'
+  resizeCanvas()
+  document.body.appendChild(canvas)
+
+  window.addEventListener('resize', () => {
+    resizeCanvas()
+  })
+  raf(render)
+  scheduleEvents()
+})
+
+function resizeCanvas () {
+  canvas.width = width = window.innerWidth
+  canvas.height = height = window.innerHeight
+}
+
+const twoPi = Math.PI * 2.0
+const numColors = 720
+const theta = twoPi / 720.0
+const startColors = ColorFactory.interpolate('#05FA8C', '#E73B57', numColors)
+const endColors = ColorFactory.interpolate('#E73B57', '#05FA8C', numColors)
+let color = 0
+let angle = 0.0
+let direction = 1
+
+function scheduleEvents () {
+  setTimeout(() => {
+    about.el = document.querySelector('#about')
+    about.el.classList = ['show']
+  }, 2000)
+}
+
+function render () {
+  const context = canvas.getContext('2d')
+  const cos = Math.cos(angle)
+  const sin = Math.sin(angle)
+  const gradient = context.createLinearGradient(0, 0, cos * width, sin * height)
+
+  gradient.addColorStop(0, startColors[color])
+  gradient.addColorStop(1, endColors[color])
+  context.fillStyle = gradient
+  context.fillRect(0, 0, width, height)
+
+  angle = angle + theta
+  if (angle > twoPi) {
+    angle -= twoPi
+  }
+
+  color += direction
+  if (color < 0 || color >= numColors) {
+    color = Math.max(Math.min(color, numColors - 1), 0)
+    direction = -direction
+  }
+
+  raf(render)
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/splash.js',
+  output: {
+    path: 'dist',
+    filename: 'splash.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel?cacheDirectory',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('css?sourceMap')
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('splash.bundle.css', {allChunks: true})
+  ]
+}
