ff96a23f46bcdabc0a92b516b44317986682c8f4
Adam DiCarlo <97462+adamdicarlo@users.noreply.github.com>
2023-04-23 21:15:26 -0700

    Deploy elm-tangly

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rm -rf wwwroot
+parcel build --dist-dir wwwroot
+
+rm -rf external
+mkdir external
+
+cd external
+git clone --depth=1 https://github.com/adamdicarlo/elm-tangly
+cd elm-tangly
+yarn --frozen-lockfile
+yarn build
+cp -au dist ../../wwwroot/elm-tangly
