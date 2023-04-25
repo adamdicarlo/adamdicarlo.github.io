7a3192b6ca307e7acdfd3926c1cdf1a4f45192ba
Adam DiCarlo <97462+adamdicarlo@users.noreply.github.com>
2023-04-24 22:13:19 -0700

    feat: neater, more subtle effect with two gradients

--- src/splash.js
+++ src/splash.js
@@ -44,11 +44,17 @@ function scheduleEvents() {
 
 const minFramesBetweenReverse = 500
 const toRadians = Math.PI / 180
-let colors = [
+let colors_ = [
   colord("#025bb7"),
   colord("#9e144a"),
   colord("#88e301"),
   colord("#ffd53c"),
+]
+let colors2_ = [
+  colord("#9e144a"),
+  colord("#aef58c"),
+  colord("#68e7f1"),
+  colord("#013ba7"),
 ]
 let frameIncrement = 1
 let framesSinceReverse = 0
@@ -56,10 +62,14 @@ let frame = 0
 let theta = 0.25
 
 function randomize() {
-  colors = Array(colors.length)
+  colors_ = Array(colors_.length)
     .fill(undefined)
     .map(() => random())
-  console.log("New colors: ", colors.map((c) => c.toHex()).join(", "))
+
+  colors2_ = Array(colors2_.length)
+    .fill(undefined)
+    .map(() => random())
+
   frameIncrement = Math.random() > 0.5 ? 1 : -1
   theta = Math.random()
 }
@@ -68,9 +78,22 @@ function render() {
   const context = canvas.getContext("2d")
 
   const angle = frame * theta
-  const radians = angle * toRadians
+  renderGradient(context, 45 + Math.cos(frame * toRadians), colors_, 1.0)
+  renderGradient(context, angle, colors2_, 0.5)
+
   frame += frameIncrement
   framesSinceReverse++
+
+  if (framesSinceReverse > minFramesBetweenReverse && Math.random() < 0.001) {
+    framesSinceReverse = 0
+    frameIncrement = -frameIncrement
+  }
+
+  window.requestAnimationFrame(render)
+}
+
+function renderGradient(context, angle, colors, alpha) {
+  const radians = angle * toRadians
 
   const cos = Math.cos(radians)
   const sin = Math.sin(radians)
@@ -85,17 +108,22 @@ function render() {
     yRotated + halfHeight
   )
 
-  gradient.addColorStop(0, colors[0].rotate(angle).toRgbString())
-  gradient.addColorStop(0.3, colors[1].rotate(angle + 90).toRgbString())
-  gradient.addColorStop(0.7, colors[3].rotate(angle - 90).toRgbString())
-  gradient.addColorStop(1.0, colors[3].rotate(-angle).toRgbString())
+  gradient.addColorStop(0, colors[0].rotate(angle).alpha(alpha).toHex())
+  gradient.addColorStop(
+    0.3,
+    colors[1]
+      .rotate(angle + 90)
+      .alpha(alpha)
+      .toHex()
+  )
+  gradient.addColorStop(
+    0.7,
+    colors[3]
+      .rotate(angle - 90)
+      .alpha(alpha)
+      .toHex()
+  )
+  gradient.addColorStop(1.0, colors[3].rotate(-angle).alpha(alpha).toHex())
   context.fillStyle = gradient
   context.fillRect(0, 0, width, height)
-
-  if (framesSinceReverse > minFramesBetweenReverse && Math.random() < 0.001) {
-    framesSinceReverse = 0
-    frameIncrement = -frameIncrement
-  }
-
-  window.requestAnimationFrame(render)
 }
